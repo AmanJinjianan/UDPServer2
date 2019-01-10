98c67d29948976439496aaefee3bdccecc81fec8
AmanJinjianan <18703698654@163.com>
2019-01-10 11:22:14 +0800

    firstupdate2

--- app/src/main/java/com/example/udpserver/SecondClass.java
+++ app/src/main/java/com/example/udpserver/SecondClass.java
@@ -1,8 +1,17 @@
 package com.example.udpserver;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
 /**
  * Created by Administrator on 2019/1/10.
  */
 
-public class SecondClass {
+public class SecondClass extends Activity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
