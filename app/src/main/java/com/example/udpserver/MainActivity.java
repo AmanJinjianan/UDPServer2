aa73ba7ba85bfa254be69f5b15ed1e4f306854ea
AmanJinjianan <18703698654@163.com>
2019-01-12 10:53:42 +0800

    firstupdate78

--- app/src/main/java/com/example/udpserver/MainActivity.java
+++ app/src/main/java/com/example/udpserver/MainActivity.java
@@ -1,24 +1,60 @@
 package com.example.udpserver;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.ChartTouchListener;
+import com.github.mikephil.charting.listener.OnChartGestureListener;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    PieChart pieChart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
-        Button the = new Button(this);
-        Button th1e = new Button(this);
+        pieChart = findViewById(R.id.pic_chart);
+        List<PieEntry> strings = new ArrayList<>();
+        strings.add(new PieEntry(30f,"aaa"));
+        strings.add(new PieEntry(70f,"bbb"));
 
-        Button th34 = new Button(this);
-        Button th56 = new Button(this);
+        PieDataSet dataSet = new PieDataSet(strings,"Label");
 
-        Button th3455 = new Button(this);
-        Button th5655 = new Button(this);
+        ArrayList<Integer> colors = new ArrayList<Integer>();
+        colors.add(Color.RED);
+        colors.add(Color.GRAY);
+        dataSet.setColors(colors);
+
+        PieData pieData = new PieData(dataSet);
+        pieData.setDrawValues(true);
+
+        pieChart.setData(pieData);
+        pieChart.invalidate();
+
     }
+
+
 }
